cbfb3bea6888876c1fabcc1424b7dc449e735dd5
Ugur Karse <ugurkrs@uoc.edu>
2025-11-05 15:10:23 +0100

    Style: Refine category page cards and add hover effects

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 console.log("¡Hola desde app.js!");
+
+// Automatic Copyright Year
+const yearSpan = document.getElementById("current-year");
+if (yearSpan) {
+  yearSpan.textContent = new Date().getFullYear();
+}
